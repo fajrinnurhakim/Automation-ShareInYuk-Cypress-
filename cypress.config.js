d117f447f22462bd7b02023cbf7e6c4804be335e
Fajrin Nurhakim <fajrinnurhakim17@gmail.com>
2024-02-23 17:01:54 +0700

    add: setting config

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,28 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    e2e: {
+        env: {
+            BASE_URL: "http://localhost:5173/",
+            email: "wartiyem123@gmail.com",
+            password: "wartiyem",
+            wrongPassword: "wartiyem123",
+            requiredPassword: "wa",
+            registerName: "wartiyemGod",
+            imageUrl:
+                "https://www.svgrepo.com/show/384674/account-avatar-profile-user-11.svg",
+            registerEmail: "wartiyem4321@gmail.com",
+            registerPassword: "wartiyem4321",
+            currentPassword: "wartiyem",
+            newPassword: "wartiyem123",
+        },
+
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
+        },
+        viewportWidth: 1200,
+        viewportHeight: 800,
+        defaultCommandTimeout: 20000,
+        requestTimeout: 20000,
     },
-  },
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -11,7 +11,20 @@
 //
 // -- This is a parent command --
 // Cypress.Commands.add('login', (email, password) => { ... })
-//
+Cypress.Commands.add("loginCommand", (email, password) => {
+    cy.get("#email").type(email);
+    cy.get("#password").type(password);
+    cy.get("input.checkbox-xs").click();
+    cy.get(".btn.btn-secondary").contains("Login").click();
+});
+
+Cypress.Commands.add("registerCommand", (name, image, email, password) => {
+    cy.get("#name").type(name);
+    cy.get("#email").type(email);
+    cy.get("#password").type(password);
+    cy.get("#agree").click();
+    cy.get(".btn.btn-secondary").contains("Register").click();
+});
 //
 // -- This is a child command --
 // Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
